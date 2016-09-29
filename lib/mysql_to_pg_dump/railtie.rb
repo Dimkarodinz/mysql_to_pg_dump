e400ab9d70d0bc4fec97909568003d88af731da9
Dimkarodinz <dimkarodin@gmail.com>
2016-09-29 21:36:03 +0300

    Added tasks to main rails app by another way

--- lib/mysql_to_pg_dump/railtie.rb
+++ lib/mysql_to_pg_dump/railtie.rb
@@ -3,8 +3,9 @@ require 'rails'
 
 module MysqlToPgDump
   class Railtie < Rails::Railtie
-    # rake_tasks do
-    #   require 'lib/tasks/db.rake'
-    # end
+    rake_tasks do
+      spec = Gem::Specification.find_by_name 'mysql_to_pg_dump'
+      require "#{spec.gem_dir}/lib/tasks/db.rake"
+    end
   end
 end
